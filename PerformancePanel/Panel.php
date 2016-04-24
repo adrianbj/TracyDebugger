aa97f05f8f1f821259c66457c8a2c3bd737795eb
adrianbj <adrianbjones@gmail.com>
2016-04-24 13:31:53 -0700

    Fix Performance Panel icon gradient

--- PerformancePanel/Panel.php
+++ PerformancePanel/Panel.php
@@ -75,12 +75,12 @@ class Panel implements IBarPanel
 				. '<span id="performance-panel" title="Performance Tracy Panel">'
 					. '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 		 					viewBox="0 0 33.1 11.7" enable-background="new 0 0 33.1 11.7" xml:space="preserve">
-							<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="5.8319" y1="0.5" x2="5.8319" y2="11.1638">
+							<linearGradient id="PerfPanelGrad" gradientUnits="userSpaceOnUse" x1="5.8319" y1="0.5" x2="5.8319" y2="11.1638">
 								<stop  offset="0" style="stop-color:#FFFFFF"/>
 								<stop  offset="1" style="stop-color:#F7A69E"/>
 							</linearGradient>
-							<rect x="0.5" y="0.5" fill="url(#SVGID_1_)" stroke="#773B37" width="10.7" height="10.7"/>
-							<rect x="22" y="0.5" fill="url(#SVGID_1_)" stroke="#773B37" width="10.7" height="10.7"/>
+							<rect x="0.5" y="0.5" fill="url(#PerfPanelGrad)" stroke="#773B37" width="10.7" height="10.7"/>
+							<rect x="22" y="0.5" fill="url(#PerfPanelGrad)" stroke="#773B37" width="10.7" height="10.7"/>
 							<polygon fill="#9ADAEA" stroke="#28A0DA" points="16.5,8.1 14,8.1 14,10.5 9,5.9 14,1.2 14,3.6 16.5,3.6 "/>
 							<polygon fill="#9ADAEA" stroke="#28A0DA" points="16.5,8.1 19,8.1 19,10.5 24.1,5.7 19.1,1.2 19.1,3.6 16.5,3.6 "/>'
 					. '</svg>'
