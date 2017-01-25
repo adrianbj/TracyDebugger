743bbb01de149613f16a66b04b3449e83bc3c821
adrianbj <adrianbjones@gmail.com>
2017-01-25 05:51:21 -0800

    Add site modules to Captain Hook and attempted fix for path and open_basedir issues.

--- CaptainHook/classes/CaptainHookSearch.php
+++ CaptainHook/classes/CaptainHookSearch.php
@@ -1,53 +1,54 @@
 <?php
 
 class CaptainHookSearch {
-	
+
 	public static function getHooks($root, $excludeFilenames = array()) {
-		
+
 		$filenamesArray = self::getPHPFilenames($root, $excludeFilenames);
-		
+
 		$hooks = array();
-		
+
 		foreach ($filenamesArray as $filename) {
 			if($hooksInFile = self::getHooksInFile($filename)) {
 				$hooks[] = $hooksInFile;
 			}
 		}
-		
+
 		return $hooks;
-		
+
 	}
-	
+
 	public static function getPHPFilenames($root, $excludeFilenames = array()) {
-		
+
 		$fileNamePattern = "/\.(php|module)$/";
-		
+
 		$iter = new RecursiveIteratorIterator(
 			new RecursiveDirectoryIterator($root, RecursiveDirectoryIterator::SKIP_DOTS),
 			RecursiveIteratorIterator::SELF_FIRST,
 			RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
 		);
 
-		$paths = array($root);
-		
+		//$paths = array($root);
+
 		foreach ($iter as $path => $dir) {
-			if (!$dir->isDir() && preg_match($fileNamePattern, $path) && !in_array(basename($path), $excludeFilenames) ) {
+			// '/.' check is for site module backups - SKIP_DOTS above is not excluding these
+			if (!$dir->isDir() && strpos($path, '/.') === false && preg_match($fileNamePattern, $path) && !in_array(basename($path), $excludeFilenames) ) {
 				$paths[] = $path;
 			}
 		}
-		
+
 		return $paths;
 	}
-	
+
 	public static function getHooksInFile($filename) {
-		
+
 		$fileContents = file_get_contents($filename);
 		$lines =  preg_split("/(\r\n|\n|\r)/", $fileContents);
 		$hookPattern = "/.+function\s*___[a-zA-Z]*\s*\(.*\)/";
 		$matches = array();
-		
+
 		$currentLine = 1;
-		
+
 		foreach ($lines as $line) {
 			if(preg_match($hookPattern, $line)) {
 				// echo $line . "<br>";
@@ -58,7 +59,7 @@ class CaptainHookSearch {
 			}
 			$currentLine++;
 		}
-		
+
 		if(count($matches) > 0) {
 			return array(
 				"filename" => $filename,
@@ -69,7 +70,7 @@ class CaptainHookSearch {
 		}
 
 	}
-	
-	
-	
+
+
+
 }
--- CaptainHook/generate-html.php
+++ CaptainHook/generate-html.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once "classes/CaptainHookSearch.php";
+require_once wire('config')->paths->TracyDebugger . 'CaptainHook/classes/CaptainHookSearch.php';
 
 $excludedFilenames = array("AdminTheme.php");
-$this->hooks = CaptainHookSearch::getHooks(wire('config')->paths->root.'wire/', $excludedFilenames);
+$wireHooks = CaptainHookSearch::getHooks(wire('config')->paths->root.'wire/', $excludedFilenames);
+$siteHooks = CaptainHookSearch::getHooks(wire('config')->paths->siteModules, $excludedFilenames);
+$this->hooks = array_merge($wireHooks, $siteHooks);
