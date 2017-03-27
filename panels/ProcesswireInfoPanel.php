acf0ae18a125cd0a8b32c9ab483e22bb5b6697a2
adrianbj <adrianbjones@gmail.com>
2017-03-26 18:38:57 -0700

    Add "Clear Session & Cookies" button to PW Info panel.

--- panels/ProcesswireInfoPanel.php
+++ panels/ProcesswireInfoPanel.php
@@ -579,7 +579,7 @@ class ProcesswireInfoPanel extends BasePanel {
         if(in_array('adminLinks', $panelSections)) {
             $out .= '
             <div class="pw-admin-links" style="text-align: center; border-top:1px solid #CCCCCC; margin-top:10px; padding-top:10px;">
-                <a href="'.$this->wire('config')->urls->admin.'" title="PW Admin">
+                <a href="'.$this->wire('config')->urls->admin.'" title="ProcessWire Admin">
                     ' . $this->icon . '
                 </a>&nbsp;
                 <a href="'.$this->wire('config')->urls->admin.'login/logout/" title="Logout">
@@ -588,6 +588,17 @@ class ProcesswireInfoPanel extends BasePanel {
                             <path d="M22.404,21.173c2.126,0,3.895-1.724,3.895-3.85V3.849C26.299,1.724,24.53,0,22.404,0c-2.126,0-3.895,1.724-3.895,3.849    v13.475C18.51,19.449,20.278,21.173,22.404,21.173z" fill="#ee1d62"></path>
                             <path d="M30.727,3.33c-0.481-0.2-1.03-0.147-1.466,0.142c-0.434,0.289-0.695,0.776-0.695,1.298v5.113    c0,0.56,0.301,1.076,0.784,1.354c4.192,2.407,6.918,6.884,6.918,11.999c0,7.654-6.217,13.882-13.87,13.882    c-7.654,0-13.86-6.228-13.86-13.882c0-5.113,2.813-9.589,6.931-11.997c0.478-0.279,0.773-0.794,0.773-1.348V4.769    c0-0.521-0.261-1.009-0.695-1.298c-0.435-0.29-0.984-0.342-1.466-0.142C6.257,6.593,0.845,14.276,0.845,23.236    c0,11.92,9.653,21.58,21.572,21.58c11.917,0,21.555-9.66,21.555-21.58C43.971,14.276,38.554,6.593,30.727,3.33z" fill="#ee1d62"></path>
                         </g>
+                    </svg>
+                </a>&nbsp;
+                <a href="'.\TracyDebugger::inputUrl(true) . (strpos(\TracyDebugger::inputUrl(true), '?') !== false ? '&' : '?') . 'tracyClearSession=1" title="Clear Session & Cookies">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
+                        <path fill="#EE2363" d="M15.747201,6.8416004h-1.9904003C13.2032013,3.5680001,10.3552008,1.072,6.9280005,1.072
+                        C3.1040001,1.072,0.0032,4.1760001,0.0032,8s3.1008,6.9280005,6.9248004,6.9280005
+                        c1.7440004,0,3.3375998-0.6464005,4.5535994-1.7087994l-1.4335995-1.8207998
+                        c-0.8224001,0.7551994-1.9167995,1.2192001-3.1167998,1.2192001c-2.5472002,0-4.6176004-2.0703993-4.6176004-4.6176004
+                        s2.0704-4.6176004,4.6176004-4.6176004c2.1472001,0,3.9487996,1.4720001,4.4640002,3.4592004H9.3792009
+                        c-0.2528,0-0.3296003,0.1632004-0.1695995,0.3583999l3.0656004,3.7728004c0.1599998,0.1984005,0.4191999,0.1984005,0.5824003,0
+                        l3.0655985-3.7728009C16.0768013,7.0048003,16,6.8416004,15.747201,6.8416004z"/>
                     </svg>
                 </a>&nbsp;
                 <a href="'.$this->wire('config')->urls->admin.'setup/template/" title="Templates">
