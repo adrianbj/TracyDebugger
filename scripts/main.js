eb779a4158272dbfda471c6e60bae8f7a6b0eb32
adrianbj <adrianbjones@gmail.com>
2018-04-11 16:02:14 -0700

    Add double-click panel rollup feature, thanks to @rolandtoth

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+function tracyResizePanel(panel, type) {
+    panel = document.getElementById("tracy-debug-panel-"+panel);
+    panel.style.left = '0px';
+    if(type == 'fullscreen') {
+        panel.style.top = '0px';
+        panel.style.height = '100vh';
+        panel.style.width = '100vw';
+    }
+    else {
+        panel.style.top = 'calc(50vh - 22px)';
+        panel.style.height = '50vh';
+        panel.style.width = '100vw';
+    }
+}
+
+// panel rollup thanks to @tpr / @rolandtoth
+// jQuery .on() equivalent
+var filterEventHandler = function (selector, callback) {
+    return (!callback || !callback.call) ? null : function (e) {
+        var target = e.target || e.srcElement || null;
+        while (target && target.parentElement && target.parentElement.querySelectorAll) {
+            var elms = target.parentElement.querySelectorAll(selector);
+            for (var i = 0; i < elms.length; i++) {
+                if (elms[i] === target) {
+                    e.filterdTarget = elms[i];
+                    callback.call(elms[i], e);
+                    return;
+                }
+            }
+            target = target.parentElement;
+        }
+    };
+};
+
+// toggle rollup state on double click
+document.addEventListener("dblclick", filterEventHandler(".tracy-panel h1", function (e) {
+    e.filterdTarget.parentElement.classList.toggle("tracy-mode-rollup");
+}));
+
+// remove rolled up state on closing the panel
+document.addEventListener("mouseup", filterEventHandler(".tracy-panel.tracy-mode-rollup [rel=\'close\']", function (e) {
+    e.filterdTarget.parentElement.parentElement.classList.remove("tracy-mode-rollup");
+}));
+
+// hide rolled up panels to allow Tracy save their position correctly
+window.addEventListener("beforeunload", function () {
+    var $rollupPanels = document.querySelectorAll(".tracy-mode-rollup");
+
+    if($rollupPanels.length) {
+        for(var i = 0; i < $rollupPanels.length; i++) {
+            $rollupPanels[i].style.visibility = "hidden";
+            $rollupPanels[i].classList.remove("tracy-mode-rollup");
+        }
+    }
+ });
