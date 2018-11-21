49b7f5c8c32a2b4408ba1642cdb60c7453eedb39
adrianbj <adrianbjones@gmail.com>
2018-11-21 14:37:54 -0800

    Update Adminer theme files and hide Logout link.

--- panels/Adminer/plugins/AdminerProcessWireLogin.php
+++ panels/Adminer/plugins/AdminerProcessWireLogin.php
@@ -4,25 +4,30 @@ $_GET['username'] = ''; // triggers autologin
 
 class AdminerProcessWireLogin {
 
-	public function __construct($server = false, $db = false, $name = false, $pass = false) {
-		$this->server = $server;
-		$this->db = $db;
-		$this->name = $name;
-		$this->pass = $pass;
-	}
+    public function __construct($server = false, $db = false, $name = false, $pass = false) {
+        $this->server = $server;
+        $this->db = $db;
+        $this->name = $name;
+        $this->pass = $pass;
+    }
 
-	function credentials() {
-		// server, username and password for connecting to database
-		return array($this->server, $this->name, $this->pass);
-	}
+    public function head() {
+    ?>
+        <link rel="stylesheet" type="text/css" href="../../../site/modules/TracyDebugger/panels/Adminer/css/tweaks.css">
+    <?php
+    }
 
-	function login() {
-		return true;
-	}
+    function credentials() {
+        // server, username and password for connecting to database
+        return array($this->server, $this->name, $this->pass);
+    }
 
-	function databases($flush = true) {
-		return [$this->db];
-	}
+    function login() {
+        return true;
+    }
+
+    function databases($flush = true) {
+        return [$this->db];
+    }
 
 }
-
