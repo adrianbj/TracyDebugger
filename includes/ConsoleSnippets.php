64d6a3836ed19011df34b7678e497e21ea99cd87
adrianbj <adrianbjones@gmail.com>
2020-04-17 22:58:27 -0700

    Fix line break (LF vs CRLF) when reloading snippets from disk after editing in external editor.

--- includes/ConsoleSnippets.php
+++ includes/ConsoleSnippets.php
@@ -10,10 +10,10 @@ if(isset($_POST['deletesnippet']) && file_exists($snippetsPath.$_POST['snippetna
     unlink($snippetsPath.$_POST['snippetname']);
 }
 elseif(isset($_POST['snippetcode'])) {
-    file_put_contents($snippetsPath.$_POST['snippetname'], \TracyDebugger::getDataValue('consoleCodePrefix') . "\n" . json_decode($_POST['snippetcode']));
+    file_put_contents($snippetsPath.$_POST['snippetname'], \TracyDebugger::getDataValue('consoleCodePrefix') . json_decode($_POST['snippetcode']));
 }
 else {
-    echo str_replace(\TracyDebugger::getDataValue('consoleCodePrefix') . "\n", '', file_get_contents($snippetsPath.$_POST['snippetname']));
+    echo str_replace(\TracyDebugger::getDataValue('consoleCodePrefix'), '', file_get_contents($snippetsPath.$_POST['snippetname']));
 }
 
 exit;
