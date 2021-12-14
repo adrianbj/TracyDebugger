203c1a6bbee8ab5e076142fbff54dc7aa3e059e0
adrianbj <adrianbjones@gmail.com>
2021-12-14 14:09:34 -0800

    Fix Issue #65

--- TracyDebugger.module.php
+++ TracyDebugger.module.php
@@ -27,7 +27,7 @@ class TracyDebugger extends WireData implements Module, ConfigurableModule {
             'summary' => __('Tracy debugger from Nette with many PW specific custom tools.', __FILE__),
             'author' => 'Adrian Jones',
             'href' => 'https://processwire.com/talk/forum/58-tracy-debugger/',
-            'version' => '4.22.20',
+            'version' => '4.22.21',
             'autoload' => 100000, // in PW 3.0.114+ higher numbers are loaded first - we want Tracy first
             'singular' => true,
             'requires'  => 'ProcessWire>=2.7.2, PHP>=5.4.4',
@@ -2561,7 +2561,7 @@ class TracyDebugger extends WireData implements Module, ConfigurableModule {
      *
      */
     public static function getDataValue($property) {
-        if(is_array(self::$_data->$property) || is_int(self::$_data->$property) || $property == 'consoleCodePrefix') {
+        if(!is_string(self::$_data->$property) || is_array(self::$_data->$property) || is_int(self::$_data->$property) || $property == 'consoleCodePrefix') {
             return self::$_data->$property;
         }
         else {
