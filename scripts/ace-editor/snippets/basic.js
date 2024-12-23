6ff4649ba05e5d6b2a02d52a086019f1e6ee146a
Adrian Jones <adrianbjones@gmail.com>
2024-12-22 17:16:13 -0800

    Update ACE

--- scripts/ace-editor/snippets/basic.js
+++ scripts/ace-editor/snippets/basic.js
@@ -1,5 +1,5 @@
 ;                (function() {
-                    window.require(["ace/theme/textmate-css"], function(m) {
+                    window.require(["ace/snippets/basic"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
