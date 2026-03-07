6ac42bc0cf220c073c04c63544ddadc972e2e732
Adrian Jones <adrianbjones@gmail.com>
2026-03-07 07:30:38 -0800

    Verify peer on CURL request to Slack

--- includes/SlackLogger.php
+++ includes/SlackLogger.php
@@ -42,7 +42,7 @@ class SlackLogger {
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         $result = curl_exec($ch);
         if (PHP_MAJOR_VERSION < 8) curl_close($ch);
         return $result;
